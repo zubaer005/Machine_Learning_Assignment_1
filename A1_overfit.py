3aa7022a83c08fd13e6796a19987666b01676230
Kabir Zubaer <zubaer005@hotmail.com>
2022-01-30 01:38:26 -0330

    command-line argument and read input file done

--- A1_overfit.py
+++ A1_overfit.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import numpy as np
+import sys
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+if(len(sys.argv)!=2):
+    print("Please provide file name")
+    exit()
+else:
+    print("File name:", sys.argv[1] )    
+    tsvFileName=sys.argv[1]
+    '''Tsv File read here'''
+    weather_df = pd.read_csv(tsvFileName, sep='\t')
+    print(weather_df)
 
-weather_df = pd.read_csv(r'C:\Users\zubae\Downloads\weatherstats_stjohns_daily.csv', parse_dates=['date'], index_col=['date']).sort_index()
-print(weather_df)
+
 
 
