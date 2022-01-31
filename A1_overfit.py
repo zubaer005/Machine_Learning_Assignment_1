e647069c5b42988104caff12026244ee45d3d428
Kabir Zubaer <zubaer005@hotmail.com>
2022-01-31 01:08:42 -0330

    referance added and unused library removed

--- A1_overfit.py
+++ A1_overfit.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import sys
-import csv
-import random
 import math
 import operator
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Machine_Learning_Assignment_1
+https://www.edureka.co/blog/k-nearest-neighbors-algorithm/
